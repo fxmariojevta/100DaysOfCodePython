3e0eb3a0228c429901109c1c821140881e5ea504
FX. Mario Jevta Prasetio <fxmariojevta@gmail.com>
2023-08-24 16:56:32 +0700

    (day 01) feat: BandNameGenerator

---
+++ day 01 (BandNameGenerator)/main.py
@@ -0,0 +1,7 @@
+if __name__ == "__main__":
+    print("Welcome to the Band Name Generator.")
+    
+    city = input("What's name of the City you grew up in?\n")
+    pet = input("What's your pet's name?\n")
+
+    print(f"Your band name could be {city} {pet}")
